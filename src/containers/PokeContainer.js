8d0d0e802b68280805f5d13b357ecbabf0b90282
Helena <inger.helena@gmail.com>
2020-09-15 15:20:51 +0100

    added another fetch

--- src/containers/PokeContainer.js
+++ src/containers/PokeContainer.js
@@ -5,11 +5,30 @@ class PokeContainer extends Component {
     constructor() {
         super();
         this.state = {
+            // pokemonUrls: [],
             pokemons: [],
             selectedPokemon: ''
         }
     }
 
+    componentDidMount() {
+        const url = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                data.results.forEach(pokemon => {
+                    fetch(pokemon.url)
+                        .then(res => res.json())
+                        .then(pokeData => this.setState(prevState => {
+                            const updatedPokemons = [...prevState.pokemons, pokeData];
+                            return { pokemons: updatedPokemons }
+                        }))
+                })
+            })
+            .catch(err => console.log(err));
+    }
+
+    
     render() {
         return(
             <>
